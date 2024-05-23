c0d14e2186a05d9c566aa1ece3f6ed95eba6f76e
ShuweiShao <77616900+ShuweiShao@users.noreply.github.com>
2024-05-23 11:58:28 +0800

    Add files via upload

--- export_gt_depth.py
+++ export_gt_depth.py
@@ -8,22 +8,20 @@ import PIL.Image as pil
 import cv2
 
 from utils import readlines
-from kitti_utils import generate_depth_map
 
-
-def export_gt_depths_kitti():
+def export_gt_depths_SCARED():
 
     parser = argparse.ArgumentParser(description='export_gt_depth')
 
     parser.add_argument('--data_path',
                         type=str,
-                        help='path to the root of the KITTI data',
+                        help='path to the root of the data',
                         required=True)
     parser.add_argument('--split',
                         type=str,
                         help='which split to export gt from',
                         required=True,
-                        choices=["eigen", "eigen_benchmark", "endovis"])
+                        choices=["endovis"])
     opt = parser.parse_args()
 
     split_folder = os.path.join(os.path.dirname(__file__), "splits", opt.split)
@@ -38,16 +36,7 @@ def export_gt_depths_kitti():
         print(i)
         print(folder)
 
-        if opt.split == "eigen":
-            calib_dir = os.path.join(opt.data_path, folder.split("/")[0])
-            velo_filename = os.path.join(opt.data_path, folder,
-                                         "velodyne_points/data", "{:010d}.bin".format(frame_id))
-            gt_depth = generate_depth_map(calib_dir, velo_filename, 2, True)
-        elif opt.split == "eigen_benchmark":
-            gt_depth_path = os.path.join(opt.data_path, folder, "proj_depth",
-                                         "groundtruth", "image_02", "{:010d}.png".format(frame_id))
-            gt_depth = np.array(pil.open(gt_depth_path)).astype(np.float32) / 256
-        elif opt.split == "endovis":
+        if opt.split == "endovis":
             f_str = "scene_points{:06d}.tiff".format(frame_id - 1)
             gt_depth_path = os.path.join(
                 opt.data_path,
@@ -68,4 +57,4 @@ def export_gt_depths_kitti():
 
 
 if __name__ == "__main__":
-    export_gt_depths_kitti()
+    export_gt_depths_SCARED()
